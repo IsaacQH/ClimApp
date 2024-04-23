ec0fdfd22df66a97752c8fc758c75544db472daa
IsaacQH <isaacqwert@gmail.com>
2024-04-22 23:05:26 -0600

    Priemra petición a API entregando clima. Variable de entorno para la key

--- src/hooks/useWeather.ts
+++ src/hooks/useWeather.ts
@@ -9,14 +9,22 @@ export default function useWeather (){
 
     const fetchWeather = async (search:SearchType) => {   //Funcion que consulta a la API los datos por medio de función asinc, correra mientras se jecutan otras cosas
 
-        const appId = 'b7366b08a97cdf5351731834042e8685'  //Id que creamos en openWeather
+        const appId = import.meta.env.VITE_API_KEY  //Id que creamos en openWeather como variable de entorno
 
         try {   //Intenta esto
 
             const geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=${appId}`
 
-            const data = await axios(geoUrl,{method:'get'})
-            console.log(data)
+            const {data} = await axios(geoUrl,{method:'get'}) //Desstructuramos solo data de la respuesta axios
+            
+            
+            const lat = data[0].lat  //Latitud 
+            const lon = data[0].lon  //Longitud
+
+            const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${appId}`
+
+            const {data:weatherResult} = await axios(weatherUrl)  //Get default -  renombramos data para evitar error
+            
 
         } catch (error) {  //Si es error, manda el error
             console.log(error)
